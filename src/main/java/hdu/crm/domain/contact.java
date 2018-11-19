a3fb99f9603884bfe62d561e2be54f92523fbadd
hyq <544608734@qq.com>
2018-11-19 22:19:02 +0800

    联系人contact类

---
+++ src/main/java/hdu/crm/domain/contact.java
@@ -0,0 +1,33 @@
+package hdu.crm.domain;
+
+public class contact {
+    
+    private String name; //联系人姓名
+    private long id;     //客户id
+    private String tel;  //联系人电话
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+}
